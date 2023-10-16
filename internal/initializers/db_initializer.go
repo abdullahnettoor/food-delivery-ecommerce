21b7dd11ecd589ce0ba2d526fe07b88a5fee204a
abdullahnettoor <abdullahnettoor@gmail.com>
2023-10-16 19:15:14 +0530

    👨‍💻 Done following things: - Converted to 3 tier architecture - Added Initializers for DB and Env - Created initial drafts of files - Restructured folder

---
+++ internal/initializers/db_initializer.go
@@ -0,0 +1,25 @@
+package initializers
+
+import (
+	"os"
+
+	"github.com/abdullahnettoor/food-delivery-ecommerce/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	var err error
+	dsn := os.Getenv("DB_URI")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(`Error connecting to DB`)
+	}
+
+}
+
+func SyncDatabase() {
+	DB.AutoMigrate(&models.Admin{}, &models.User{}, &models.Restaurant{})
+}
---
+++ internal/middlewares/auth_middleware.go
@@ -0,0 +1,12 @@
+package middlewares
+
+import "github.com/gofiber/fiber/v2"
+
+// Authourize admin
+func AuthorizeAdmin(f *fiber.Ctx) {
+
+}
+
+// Authorize restaurant
+
+// Authorize user
